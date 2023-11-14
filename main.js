0e1670b1a064b6ccc58141da78ad3847109e01a6
Sherief10 <sheriefsaad10@gmail.com>
2023-11-14 09:09:36 +0200

    update code to ES6 class syntax

--- main.js
+++ main.js
@@ -1,50 +1,53 @@
-let myLibrary = [];
-
-function Book(title, author, pages, read) {
-  // the constructor
-  this.title = title;
-  this.author = author;
-  this.pages = pages;
-  this.read = read;
-}
-
-function render() {
-  let libraryEl = document.querySelector("#library");
-  libraryEl.innerHTML = "";
-  for (let i = 0; i < myLibrary.length; i++) {
-    let book = myLibrary[i];
-    let bookEl = document.createElement("div");
-    bookEl.setAttribute("class", "book-card");
-    bookEl.innerHTML = `
-    <div class="card-header">
-      <h3 class="title">${book.title}</h3>
-      <h5 class="author">by ${book.author}</h5>
-      </div>
-    <div class="card-body">
-      <p>${book.pages} pages</p>
-      <p class="read-status">${book.read ? "Read" : "Not Read Yet"}</p>
-      <button class="remove-btn" onclick="removeBook(${i})">Remove</button>
-    </div>
-    `;
-
-    libraryEl.appendChild(bookEl);
+class Book {
+  constructor(title, author, pages, read) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
   }
 }
 
-function removeBook(index) {
-  myLibrary.splice(index, 1);
-  render();
+class Library {
+  constructor() {
+    this.myLibrary = [];
+  }
+
+  render() {
+    let libraryEl = document.querySelector("#library");
+    libraryEl.innerHTML = "";
+    for (let i = 0; i < this.myLibrary.length; i++) {
+      let book = this.myLibrary[i];
+      let bookEl = document.createElement("div");
+      bookEl.setAttribute("class", "book-card");
+      bookEl.innerHTML = `
+        <div class="card-header">
+          <h3 class="title">${book.title}</h3>
+          <h5 class="author">by ${book.author}</h5>
+        </div>
+        <div class="card-body">
+          <p>${book.pages} pages</p>
+          <p class="read-status">${book.read ? "Read" : "Not Read Yet"}</p>
+          <button class="remove-btn" onclick="removeBook(${i})">Remove</button>
+        </div>
+      `;
+
+      libraryEl.appendChild(bookEl);
+    }
+  }
+
+  removeBook(index) {
+    this.myLibrary.splice(index, 1);
+    this.render();
+  }
+
+  addBookToLibrary(title, author, pages, read) {
+    let newBook = new Book(title, author, pages, read);
+    this.myLibrary.push(newBook);
+    this.render();
+  }
 }
 
-function addBookToLibrary() {
-  let title = document.querySelector("#title").value;
-  let author = document.getElementById("author").value;
-  let pages = document.getElementById("pages").value;
-  let read = document.getElementById("read").checked;
-  let newBook = new Book(title, author, pages, read);
-  myLibrary.push(newBook);
-  render();
-}
+let library = new Library();
 
 let newBookbtn = document.querySelector("#new-book-btn");
 newBookbtn.addEventListener("click", function () {
@@ -56,5 +59,9 @@ document
   .querySelector("#new-book-form")
   .addEventListener("submit", function (event) {
     event.preventDefault();
-    addBookToLibrary();
+    let title = document.querySelector("#title").value;
+    let author = document.getElementById("author").value;
+    let pages = document.getElementById("pages").value;
+    let read = document.getElementById("read").checked;
+    library.addBookToLibrary(title, author, pages, read);
   });
